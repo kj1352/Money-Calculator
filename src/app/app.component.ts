30f4e85e38c8995a007cbea0bbbc8ab80eaf8792
kj1352 <nikhilkj24@gmail.com>
2018-12-07 01:04:54 +0530

    Initial Commit with calculator and stats page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,18 @@ import { HomePage } from '../pages/home/home';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      platform.registerBackButtonAction(() => {
+        //sometimes the best thing you can do is not think, not wonder, not imagine, not obsess.
+        //just breathe, and have faith that everything will work out for the best.
+      },1);
+
     });
   }
 }
-
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,156 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    total = 0;
+    items_list = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+    public alertCtrl: AlertController,
+    public actionSheetCtrl: ActionSheetController) {
 
+        if (localStorage.items_list === undefined || localStorage.items_list === null) {
+            console.log("No items");
+        } else {
+            this.items_list = JSON.parse(localStorage.items_list);
+            this.calculateTotal();
+        }
+
+  }
+
+  deletePrompt(i) {
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            this.deleteItem(i);
+          }
+        }, {
+          text: 'Edit',
+          handler: () => {
+            this.editItem(i);
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
+  editItem(i) {
+      let prompt = this.alertCtrl.create({
+        title: 'Edit Item',
+        message: "Edit the expense details",
+        inputs: [
+          {
+            name: 'title',
+            placeholder: 'Title',
+            value: this.items_list[i].title
+          },
+          {
+            name: 'amount',
+            placeholder: 'Amount',
+            type: 'tel',
+            value: this.items_list[i].amount
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          },
+          {
+            text: 'Save',
+            handler: data => {
+              this.editEntry(i, data.title, data.amount);
+            }
+          }
+        ]
+      });
+      prompt.present();
+  }
+
+
+  addToLog() {
+    let prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Enter the expense details",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+        {
+          name: 'amount',
+          placeholder: 'Amount',
+          type: 'tel'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.addToEntry(data.title, data.amount);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  formatDate(date) {
+      return moment(date).fromNow();
+  }
+
+  deleteItem(i) {
+      this.items_list.splice(i, 1);
+      localStorage.items_list = JSON.stringify(this.items_list);
+      this.calculateTotal();
+  }
+
+
+  addToEntry(title, amount) {
+      this.items_list.push({title: title, amount : amount, date: new Date() });
+      localStorage.items_list = JSON.stringify(this.items_list);
+      this.calculateTotal();
+      console.log(this.items_list);
+  }
+
+  editEntry(i, title, amount) {
+      this.items_list[i].title = title;
+      this.items_list[i].amount = amount;
+      this.items_list[i].date = new Date();
+      localStorage.items_list = JSON.stringify(this.items_list);
+      this.calculateTotal();
+      console.log(this.items_list);
+  }
+
+  calculateTotal() {
+      this.total = 0;
+
+      if (this.items_list.length > 0) {
+          for (var i = 0; i < this.items_list.length; i += 1) {
+              this.total = Number(this.total) + Number(this.items_list[i].amount);
+          }
+      } else {
+          this.total = 0;
+      }
   }
 
 }
